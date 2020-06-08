f3a6fa53401931272f049ced8ffbaa80485686d2
nato-x <alvares.renato@hotmail.com>
2020-06-08 19:17:40 -0300

    Small fix on data-testid of title-settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Settings() {
   return (
     <div>
-      <h1 data-testid="setting-title">settings</h1>
+      <h1 data-testid="settings-title">settings</h1>
     </div>
   );
 }

45757e9ce06a4a29c5f774cce0726151017389d0
luis.campos <gucampos845@gmail.com>
2020-06-10 14:35:55 -0300

    Teste gravatar

--- src/components/Play.js
+++ src/components/Play.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 
 class Play extends React.Component {
   render() {
-    console.log(this.props.questions);
     return (
       <div>
-        jogo
+        <img
+          data-testid="header-profile-picture"
+          src="none"
+          d="https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3"
+          alt="Foto Perfil"
+        />
+        <h4 data-testid="header-player-name">Player</h4>
+        <span data-testid="header-score">0</span>
       </div>
     );
   }
